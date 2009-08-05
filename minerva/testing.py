20cac4e6a8c5e2bab7265cc4150061918696d5a9
ivank <ivank@ludios.net>
2009-08-05 13:26:41 -0700

    add testing page for later

---
+++ minerva/testing.py
@@ -0,0 +1,19 @@
+from twisted.web import
+
+from cwtools import testing
+
+
+class MinervaTestPage(testing.TestPage):
+	testPackage = 'CW.Net.Test'
+
+
+
+class Index(resource.Resource):
+
+	def __init__(self, reactor, qFinder):
+		self._reactor = reactor
+		self._qFinder = qFinder
+
+		resource.Resource.__init__(self)
+		self.putChild('@tests', MinervaTestPage())
+		###self.putChild('d', XHRStream(self._reactor, self._qFinder))
