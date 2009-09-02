9d1d30ee66c3fd35b0433adcda5ed56adbdc6d6d
ivank <ivank@ludios.net>
2009-09-02 03:09:25 -0700

    apply optimization that combines readLength and mode variable

--- js/CW/Net/__init__.js
+++ js/CW/Net/__init__.js
@@ -17,12 +17,11 @@ CW.Class.subclass(CW.Net, "ResponseTextDecoder").methods(
 	 * or any object with a unicode C{responseText} property.
 	 */
 	function __init__(self, xObject, MAX_LENGTH) {
-		// TODO: as an ugly optimization, mode and readLength could be combined.
 		self._offset = 0;
 		// Need to have at least 1 byte before doing any parsing
 		self._ignoreUntil = 1;
-		self._mode = 0; // 0 means LENGTH, 1 means DATA
-		self._readLength = null;
+		// Optimization hack: this acts as both a mode and a readLength
+		self._readLengthMode = 0; // 0 means mode LENGTH, >= 1 means mode DATA
 		self.xObject = xObject;
 		if(!MAX_LENGTH) {
 			MAX_LENGTH = 1024*1024*1024;
@@ -60,7 +59,7 @@ CW.Class.subclass(CW.Net, "ResponseTextDecoder").methods(
 		}
 		var strings = [];
 		for(;;) {
-			if(self._mode === 0) { // mode LENGTH
+			if(self._readLengthMode === 0) { // mode LENGTH
 				var colon = text.indexOf(':', self._offset);
 				if(colon === -1) {
 					if(responseTextLength - self._offset > self.MAX_LENGTH_LEN) {
@@ -84,26 +83,24 @@ CW.Class.subclass(CW.Net, "ResponseTextDecoder").methods(
 				if(readLength > self.MAX_LENGTH) {
 					throw new CW.Net.ParseError("length too long: " + readLength);
 				}
-				self._readLength = readLength;
+				self._readLengthMode = readLength;
 				self._offset += (''+readLength).length + 1; // + 1 to skip over the ":"
-				self._mode = 1;
 			} else { // mode DATA
-				if(self._offset + self._readLength > responseTextLength) {
+				if(self._offset + self._readLengthMode > responseTextLength) {
 					////console.log('Not enough data bytes yet. Break.');
 					break;
 				}
-				var s = text.substr(self._offset, self._readLength);
-				self._offset += self._readLength;
-				//self._readLength = null; // reseting this is optional
-				self._mode = 0;
+				var s = text.substr(self._offset, self._readLengthMode);
+				self._offset += self._readLengthMode;
+				self._readLengthMode = 0;
 				strings.push(s);
 			}
 		}
-		if(self._mode === 0) {
+		if(self._readLengthMode === 0) {
 			// Can't ignore anything when still receiving the length
 			self._ignoreUntil = responseTextLength + 1;
 		} else {
-			self._ignoreUntil = self._offset + self._readLength;
+			self._ignoreUntil = self._offset + self._readLengthMode;
 		}
 		////console.log('_ignoreUntil now', self._ignoreUntil);
 		return strings;
