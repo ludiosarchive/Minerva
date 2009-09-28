7177db7154bbc9826d725c04ab4be9690de9c527
ivank <ivank@ludios.net>
2009-09-28 14:18:19 -0700

    rearrange tests; make XDR-using tests run in just IE

--- js/CW/Net/TestNet.js
+++ js/CW/Net/TestNet.js
@@ -61,6 +61,139 @@ CW.UnitTest.TestCase.subclass(CW.Net.TestNet, 'FindObjectTests').methods(
 );
 
 
+/**
+ * These tests do not make any real connections.
+ */
+CW.UnitTest.TestCase.subclass(CW.Net.TestNet, 'ReusableXHRLogicTests').methods(
+
+	function setUp(self) {
+		self.target = CW.URI.URL(''+window.location).update('fragment', null);
+	},
+
+
+	function _setupDummies(self) {
+		self.target.update('path', '/@testres_Minerva/404/');
+		self.mock = CW.Net.TestNet.MockXHR();
+		self.assertIdentical(CW.emptyFunc, self.mock.onreadystatechange);
+		self.xhr = CW.Net.ReusableXHR(window, self.mock);
+		self.requestD = self.xhr.request('POST', self.target, '');
+		// After .request(), onreadystatechange is set to a real handler.
+		self.assertNotIdentical(CW.emptyFunc, self.mock.onreadystatechange);
+	},
+
+
+	/**
+	 * We can't make another request over this ReusableXHR
+	 * object until the current request is finished.
+	 */
+	function test_requestStillActive(self) {
+		self._setupDummies();
+		self.assertThrows(
+			CW.Net.RequestStillActive,
+			function() { self.xhr.request('GET', self.target) },
+			"Wait for the Deferred to fire before making another request."
+		);
+	},
+
+
+	/**
+	 * After aborting, using the same L{ReusableXHR} instance to make
+	 * requests still works.
+	 */
+	function test_abortDoesntRuinState(self) {
+		self._setupDummies();
+		self.xhr.abort();
+		self.mock.readyState = 4;
+		self.mock.onreadystatechange(null);
+
+		// Make the second request
+		self.requestD = self.xhr.request('POST', self.target, 'second');
+		self.mock.readyState = 4;
+		self.mock.onreadystatechange(null);
+
+		CW.msg('log has length ' + self.mock.log.length);
+		CW.msg(CW.UnitTest.repr(self.mock.log));
+		self.assertEqual([
+			['open', 'POST', self.target.getString(), true], ['send', ''], ['abort'],
+			['open', 'POST', self.target.getString(), true], ['send', 'second']
+		], self.mock.log);
+	},
+
+
+	/**
+	 * The request Deferred errback fires with error L{CW.Net.RequestAborted}.
+	 *
+	 * We use a dummy because some browsers finish a request so fast
+	 * after .request(), that .abort() becomes a no-op (notably Opera 9/10, Safari 3)
+	 */
+	function test_abortReason(self) {
+		self._setupDummies();
+		self.assertEqual(self.mock.log, [['open', 'POST', self.target.getString(), true], ['send', '']]);
+
+		var d = self.assertFailure(self.requestD, [CW.Net.RequestAborted]);
+
+		self.xhr.abort();
+		self.mock.readyState = 4;
+		self.mock.onreadystatechange(null);
+
+		return d;
+	},
+
+
+	/**
+	 * The C{abort} method of the XHR object is only called once, even if
+	 * L{ReusableXHR.abort} is called multiple times.
+	 *
+	 * We use a dummy because calling .abort() multiple times on a browser's
+	 * XHR object doesn't raise an exception or do anything important. We still
+	 * want to prevent it from happening.
+	 */
+	function test_abortCalledOnlyOnce(self) {
+		self._setupDummies();
+		self.xhr.abort();
+		self.xhr.abort();
+		self.assertEqual(self.mock.log, [['open', 'POST', self.target.getString(), true], ['send', ''], ['abort']]);
+	},
+
+
+	/**
+	 * The implementations clears the C{onreadystatechange} property
+	 * of the XHR object after the request is _done_. This is necessary to
+	 * avoid memory leaks in IE.
+	 */
+	function test_onreadystatechangeResetAfterFinish(self) {
+		self._setupDummies();
+
+		// Finish the request and verify that onreadystatechange was
+		// reset to L{CW.emptyFunc}
+		self.mock.readyState = 4;
+		self.mock.responseText = 'done';
+		self.mock.onreadystatechange(null);
+		self.assertIdentical(CW.emptyFunc, self.mock.onreadystatechange);
+	},
+
+
+	/**
+	 * The implementations clears the C{onreadystatechange} property
+	 * of the XHR object after the request is _aborted_. This is necessary to
+	 * avoid memory leaks in IE.
+	 */
+	function test_onreadystatechangeResetAfterAborted(self) {
+		self._setupDummies();
+
+		// Abort the request and verify that onreadystatechange was
+		// reset to L{CW.emptyFunc}
+		self.xhr.abort();
+		self.mock.readyState = 4;
+		self.mock.responseText = 'aborted';
+		self.mock.onreadystatechange(null);
+		self.assertIdentical(CW.emptyFunc, self.mock.onreadystatechange);
+	}
+
+	// TODO: test progressCallback
+);
+
+
 
 CW.UnitTest.TestCase.subclass(CW.Net.TestNet, 'ReusableXHRTests').methods(
 
@@ -213,16 +346,20 @@ CW.UnitTest.TestCase.subclass(CW.Net.TestNet, 'ReusableXHRTests').methods(
 );
 
 
-
-
-CW.Net.TestNet.ReusableXHRTests.subclass(CW.Net.TestNet, 'ReusableXHRStreamingTests').methods(
+/**
+ * Run L{ReusableXHRTests} except with the XDR object, if it's available in this
+ * browser.
+ */
+CW.Net.TestNet.ReusableXHRTests.subclass(CW.Net.TestNet, 'ReusableXHRUsingXDRTests').methods(
 
 	function setUp(self) {
+		if(!CW.Net.TestNet.hasXDomainRequest()) {
+			throw new CW.UnitTest.SkipTest("XDomainRequest is required for this test.");
+		}
 		self.target = CW.URI.URL(''+window.location);
 		self.xhr = CW.Net.ReusableXHR(window, CW.Net.findObject(/*desireXDR=*/true));
 	}
 );
-
 
 
 
@@ -350,132 +487,3 @@ CW.UnitTest.TestCase.subclass(CW.Net.TestNet, 'OperaWorkaroundTests').methods(
 );
 
 
-
-CW.UnitTest.TestCase.subclass(CW.Net.TestNet, 'ReusableXHRLogicTests').methods(
-
-	function setUp(self) {
-		self.target = CW.URI.URL(''+window.location).update('fragment', null);
-	},
-
-
-	function _setupDummies(self) {
-		self.target.update('path', '/@testres_Minerva/404/');
-		self.mock = CW.Net.TestNet.MockXHR();
-		self.assertIdentical(CW.emptyFunc, self.mock.onreadystatechange);
-		self.xhr = CW.Net.ReusableXHR(window, self.mock);
-		self.requestD = self.xhr.request('POST', self.target, '');
-		// After .request(), onreadystatechange is set to a real handler.
-		self.assertNotIdentical(CW.emptyFunc, self.mock.onreadystatechange);
-	},
-
-
-	/**
-	 * We can't make another request over this ReusableXHR
-	 * object until the current request is finished.
-	 */
-	function test_requestStillActive(self) {
-		self._setupDummies();
-		self.assertThrows(
-			CW.Net.RequestStillActive,
-			function() { self.xhr.request('GET', self.target) },
-			"Wait for the Deferred to fire before making another request."
-		);
-	},
-
-
-	/**
-	 * After aborting, using the same L{ReusableXHR} instance to make
-	 * requests still works.
-	 */
-	function test_abortDoesntRuinState(self) {
-		self._setupDummies();
-		self.xhr.abort();
-		self.mock.readyState = 4;
-		self.mock.onreadystatechange(null);
-
-		// Make the second request
-		self.requestD = self.xhr.request('POST', self.target, 'second');
-		self.mock.readyState = 4;
-		self.mock.onreadystatechange(null);
-
-		CW.msg('log has length ' + self.mock.log.length);
-		CW.msg(CW.UnitTest.repr(self.mock.log));
-		self.assertEqual([
-			['open', 'POST', self.target.getString(), true], ['send', ''], ['abort'],
-			['open', 'POST', self.target.getString(), true], ['send', 'second']
-		], self.mock.log);
-	},
-
-
-	/**
-	 * The request Deferred errback fires with error L{CW.Net.RequestAborted}.
-	 *
-	 * We use a dummy because some browsers finish a request so fast
-	 * after .request(), that .abort() becomes a no-op (notably Opera 9/10, Safari 3)
-	 */
-	function test_abortReason(self) {
-		self._setupDummies();
-		self.assertEqual(self.mock.log, [['open', 'POST', self.target.getString(), true], ['send', '']]);
-
-		var d = self.assertFailure(self.requestD, [CW.Net.RequestAborted]);
-
-		self.xhr.abort();
-		self.mock.readyState = 4;
-		self.mock.onreadystatechange(null);
-
-		return d;
-	},
-
-
-	/**
-	 * The C{abort} method of the XHR object is only called once, even if
-	 * L{ReusableXHR.abort} is called multiple times.
-	 *
-	 * We use a dummy because calling .abort() multiple times on a browser's
-	 * XHR object doesn't raise an exception or do anything important. We still
-	 * want to prevent it from happening.
-	 */
-	function test_abortCalledOnlyOnce(self) {
-		self._setupDummies();
-		self.xhr.abort();
-		self.xhr.abort();
-		self.assertEqual(self.mock.log, [['open', 'POST', self.target.getString(), true], ['send', ''], ['abort']]);
-	},
-
-
-	/**
-	 * The implementations clears the C{onreadystatechange} property
-	 * of the XHR object after the request is _done_. This is necessary to
-	 * avoid memory leaks in IE.
-	 */
-	function test_onreadystatechangeResetAfterFinish(self) {
-		self._setupDummies();
-
-		// Finish the request and verify that onreadystatechange was
-		// reset to L{CW.emptyFunc}
-		self.mock.readyState = 4;
-		self.mock.responseText = 'done';
-		self.mock.onreadystatechange(null);
-		self.assertIdentical(CW.emptyFunc, self.mock.onreadystatechange);
-	},
-
-
-	/**
-	 * The implementations clears the C{onreadystatechange} property
-	 * of the XHR object after the request is _aborted_. This is necessary to
-	 * avoid memory leaks in IE.
-	 */
-	function test_onreadystatechangeResetAfterAborted(self) {
-		self._setupDummies();
-
-		// Abort the request and verify that onreadystatechange was
-		// reset to L{CW.emptyFunc}
-		self.xhr.abort();
-		self.mock.readyState = 4;
-		self.mock.responseText = 'aborted';
-		self.mock.onreadystatechange(null);
-		self.assertIdentical(CW.emptyFunc, self.mock.onreadystatechange);
-	}
-
-	// TODO: test progressCallback
-);
