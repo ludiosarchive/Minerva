2308df543641b726e77ca8db1660eff0d6e4bda9
unknown <Administrator2@.(none)>
2009-10-27 11:33:22 -0700

    move things around

--- minerva/test_newlink.py
+++ minerva/test_newlink.py
@@ -6,8 +6,7 @@ from twisted.internet import reactor, protocol, defer, address, interfaces, task
 from twisted.web.test.test_web import DummyRequest as _TwistedDummyRequest
 
 from minerva.newlink import (
-	Frame, BadFrame, IMinervaProtocol, IMinervaFactory, BasicMinervaProtocol, BasicMinervaFactory,
-	ICsrfStopper, CsrfStopper, RejectToken
+	Frame, BadFrame, IMinervaProtocol, IMinervaFactory, BasicMinervaProtocol, BasicMinervaFactory
 )
 
 from minerva.website import (
@@ -154,88 +153,6 @@ class FrameTests(unittest.TestCase):
 	def test_repr(self):
 		f = Frame([0, u"hello"])
 		self.assertEqual("<Frame type 'boxes', contents [0, u'hello']>", repr(f))
-
-
-
-class CsrfStopperTests(unittest.TestCase):
-
-	def test_implements(self):
-		verify.verifyObject(ICsrfStopper, CsrfStopper("secret string"))
-
-
-	def test_makeTokenType(self):
-		c = CsrfStopper("secret string")
-		i = _DummyId("id")
-		self.assertIdentical(str, type(c.makeToken(i)))
-
-
-	def test_makeTokenDifferentForDifferentSecret(self):
-		c1 = CsrfStopper("secret string")
-		i1 = _DummyId("id")
-
-		c2 = CsrfStopper("secret string 2")
-		i2 = _DummyId("id")
-
-		self.assertNotEqual(i1, i2)
-
-
-	def test_makeTokenDifferentForDifferentId(self):
-		c1 = CsrfStopper("secret string")
-		i1 = _DummyId("id")
-
-		c2 = CsrfStopper("secret string")
-		i2 = _DummyId("id 2")
-
-		self.assertNotEqual(i1, i2)
-
-
-	def test_makeTokenMakesSafeBase64(self):
-		import base64
-		c = CsrfStopper("secret string")
-		i = _DummyId("id")
-		token = c.makeToken(i)
-		# no error
-		base64.urlsafe_b64decode(token)
-
-
-	def test_makeTokenMakes160Bits(self):
-		import base64
-		c = CsrfStopper("secret string")
-		i = _DummyId("id")
-		token = c.makeToken(i)
-		decoded = base64.urlsafe_b64decode(token)
-		self.assertEqual(160, len(decoded) * 8)
-
-
-	def test_checkTokenWorks(self):
-		c = CsrfStopper("secret string")
-		i = _DummyId("id")
-
-		token = c.makeToken(i)
-		# no exception
-		c.checkToken(i, token)
-
-		# wrong uuid
-		differentI = _DummyId("id 2")
-		self.assertRaises(RejectToken, lambda: c.checkToken(differentI, token))
-
-		badToken = 'AAA' + token # still valid base64
-		self.assertRaises(RejectToken, lambda: c.checkToken(i, badToken))
-
-
-	def test_checkTokenCorruptBase64(self):
-		c = CsrfStopper("secret string")
-		i = _DummyId("id")
-		token = c.makeToken(i)
-
-		self.assertRaises(RejectToken, lambda: c.checkToken(i, 'x' + token))
-		self.assertRaises(RejectToken, lambda: c.checkToken(i, 'xx' + token))
-
-		# Surprise, all of these are still "valid" according to Python
-		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + 'x'))
-		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + '='))
-		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + '=='))
-		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + '==='))
 
 
 
--- minerva/test_website.py
+++ minerva/test_website.py
@@ -1,13 +1,95 @@
 from zope.interface import implements, verify
 from twisted.trial import unittest
 
-from minerva.newlink import CsrfStopper
-from minerva.test_newlink import DummyRequest, DummyHttpTransport
+from minerva.test_newlink import DummyRequest, DummyHttpTransport, _DummyId
 
 from minerva.website import (
 	RejectTransport, ITransportFirewall, CsrfTransportFirewall,
-	NoopTransportFirewall, AntiHijackTransportFirewall
+	NoopTransportFirewall, AntiHijackTransportFirewall,
+	ICsrfStopper, CsrfStopper, RejectToken
 )
+
+
+
+class CsrfStopperTests(unittest.TestCase):
+
+	def test_implements(self):
+		verify.verifyObject(ICsrfStopper, CsrfStopper("secret string"))
+
+
+	def test_makeTokenType(self):
+		c = CsrfStopper("secret string")
+		i = _DummyId("id")
+		self.assertIdentical(str, type(c.makeToken(i)))
+
+
+	def test_makeTokenDifferentForDifferentSecret(self):
+		c1 = CsrfStopper("secret string")
+		i1 = _DummyId("id")
+
+		c2 = CsrfStopper("secret string 2")
+		i2 = _DummyId("id")
+
+		self.assertNotEqual(i1, i2)
+
+
+	def test_makeTokenDifferentForDifferentId(self):
+		c1 = CsrfStopper("secret string")
+		i1 = _DummyId("id")
+
+		c2 = CsrfStopper("secret string")
+		i2 = _DummyId("id 2")
+
+		self.assertNotEqual(i1, i2)
+
+
+	def test_makeTokenMakesSafeBase64(self):
+		import base64
+		c = CsrfStopper("secret string")
+		i = _DummyId("id")
+		token = c.makeToken(i)
+		# no error
+		base64.urlsafe_b64decode(token)
+
+
+	def test_makeTokenMakes160Bits(self):
+		import base64
+		c = CsrfStopper("secret string")
+		i = _DummyId("id")
+		token = c.makeToken(i)
+		decoded = base64.urlsafe_b64decode(token)
+		self.assertEqual(160, len(decoded) * 8)
+
+
+	def test_checkTokenWorks(self):
+		c = CsrfStopper("secret string")
+		i = _DummyId("id")
+
+		token = c.makeToken(i)
+		# no exception
+		c.checkToken(i, token)
+
+		# wrong uuid
+		differentI = _DummyId("id 2")
+		self.assertRaises(RejectToken, lambda: c.checkToken(differentI, token))
+
+		badToken = 'AAA' + token # still valid base64
+		self.assertRaises(RejectToken, lambda: c.checkToken(i, badToken))
+
+
+	def test_checkTokenCorruptBase64(self):
+		c = CsrfStopper("secret string")
+		i = _DummyId("id")
+		token = c.makeToken(i)
+
+		self.assertRaises(RejectToken, lambda: c.checkToken(i, 'x' + token))
+		self.assertRaises(RejectToken, lambda: c.checkToken(i, 'xx' + token))
+
+		# Surprise, all of these are still "valid" according to Python
+		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + 'x'))
+		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + '='))
+		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + '=='))
+		##self.assertRaises(RejectToken, lambda: c.checkToken(i, token + '==='))
 
 
 
