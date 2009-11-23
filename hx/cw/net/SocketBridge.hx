332441ef73e9f7306e9404610d84e86d0f7a5e2b
ivank <ivank@ludios.net>
2009-11-23 05:53:30 -0800

    make it one big string

--- hx/cw/net/SocketBridge.hx
+++ hx/cw/net/SocketBridge.hx
@@ -111,49 +111,51 @@ class SocketBridge {
 		ExternalInterface.addCallback("close", close);	
 		ExternalInterface.addCallback("write", write);	
 		ExternalInterface.addCallback("CAN_I_HAS_SOCKET", CAN_I_HAS_SOCKET);
-		ExternalInterface.call([
-		"(function(){",
-			"if (window.FlashSocket) return;",
-			"var Class = function(properties){",
-				"var klass = function(event_handlers){ ",
-					"for (var p in event_handlers) {",
-						"if (event_handlers.hasOwnProperty(p)) {",
-							"this[p] = event_handlers[p];",
-						"}",
-					"}",
-					"return this.init.apply(this);",
-				"};",
-				"klass.prototype = properties;",
-				"klass.constructor = arguments.callee;",
-				"return klass;",
-			"};",
-			"window.FlashSocket = new Class({",
-				"init: function(){",
-					"this._instance = ''+window.FlashSocket._instances.length;",
-					"window.FlashSocket._instances.push(this);",
-				"},",
-				"close: function(){ ",
-					"window.FlashSocket._instances[this._instance] = null;",
-					"window.FlashSocket._bridge.close(this._instance );",
-				"},",
-				"write: function(data){ ",
-					"window.FlashSocket._bridge.write(this._instance, data);",
-				"},",
-				"connect: function(host, port) {",
-					"window.FlashSocket._bridge.connect(this._instance, host, port);",
-				"},",
-				"loadPolicyFile: function(path) {",
-					"window.FlashSocket._bridge.loadPolicyFile(path);",
-				"}",
-			"});",
-			"window.FlashSocket._instances = [];",
-			"var f = function(tag){",
-				"var elems = document.getElementsByTagName(tag);",
-				"for (var i=0; i<elems.length; i++) if (elems[i].CAN_I_HAS_SOCKET) return elems[i];",
-			"};",
-			"window.FlashSocket._bridge = f('embed') || f('object');",
-		"})" ].join('') );
-		if (flash.Lib.current.loaderInfo.parameters.onloadcallback != null)
+		// Despite possibly-incorrect syntax highlighting below, it's really JavaScript code, not haXe code.
+		ExternalInterface.call("
+		(function(){
+			if (window.FlashSocket) return;
+			var Class = function(properties){
+				var klass = function(event_handlers){
+					for (var p in event_handlers) {
+						if (event_handlers.hasOwnProperty(p)) {
+							this[p] = event_handlers[p];
+						}
+					}
+					return this.init.apply(this);
+				};
+				klass.prototype = properties;
+				klass.constructor = arguments.callee;
+				return klass;
+			};
+			window.FlashSocket = new Class({
+				init: function(){
+					this._instance = ''+window.FlashSocket._instances.length;
+					window.FlashSocket._instances.push(this);
+				},
+				close: function(){
+					window.FlashSocket._instances[this._instance] = null;
+					window.FlashSocket._bridge.close(this._instance );
+				},
+				write: function(data){
+					window.FlashSocket._bridge.write(this._instance, data);
+				},
+				connect: function(host, port) {
+					window.FlashSocket._bridge.connect(this._instance, host, port);
+				},
+				loadPolicyFile: function(path) {
+					window.FlashSocket._bridge.loadPolicyFile(path);
+				}
+			});
+			window.FlashSocket._instances = [];
+			var f = function(tag){
+				var elems = document.getElementsByTagName(tag);
+				for (var i=0; i<elems.length; i++) if (elems[i].CAN_I_HAS_SOCKET) return elems[i];
+			};
+			window.FlashSocket._bridge = f('embed') || f('object');
+		})");
+		if (flash.Lib.current.loaderInfo.parameters.onloadcallback != null) {
 			ExternalInterface.call(flash.Lib.current.loaderInfo.parameters.onloadcallback);
+		}
 	}
 }
