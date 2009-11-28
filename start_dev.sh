6f1e64da0c82cb4b7bfec9dbe8a27344293a93db
ivank <ivank@ludios.net>
2009-11-28 00:54:52 -0800

    add start_dev.sh

---
+++ start_dev.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+export JSPATH="$HOME/JSPATH"
+export PYTHONPATH=$HOME/Projects/Coreweb:$HOME/Projects/Minerva
+export PYRELOADING=1
+
+echo "Using `which twistd`"
+
+looper twistd -no minervarun -a tcp:8111:interface=0 -b ssl:444:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=0
