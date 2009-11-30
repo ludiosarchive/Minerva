e72b147da4d8577c1481ee37a6135ae8fa36a28e
ivank <ivank@ludios.net>
2009-11-30 02:52:41 -0800

    write more on README; improve compile.sh; move flash_disassembly to Coreweb

--- compile.sh
+++ compile.sh
@@ -3,3 +3,4 @@ export HAXE_LIBRARY_PATH=/usr/local/haxe/std:./hx
 export HAXE_HOME=/usr/local/haxe
 PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
 haxe -v -swf-version 9 -swf ./minerva/flash_build/app.swf -main cw.net.SocketBridge > ./minerva/flash_build/build.log
+cp /med/builds/swfobject/swfobject/expressInstall.swf ./cwtools/exp/
