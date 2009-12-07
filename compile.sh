eee6ed54393a97385e72fa744b962f8a916683a5
ivank <ivank@ludios.net>
2009-12-06 18:52:21 -0800

    make compile.sh log date

--- compile.sh
+++ compile.sh
@@ -2,5 +2,7 @@
 export HAXE_LIBRARY_PATH=/usr/local/haxe/std:./hx
 export HAXE_HOME=/usr/local/haxe
 PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
-haxe -v -swf-version 9 -swf ./minerva/flash_build/app.swf -main cw.net.SocketBridge > ./minerva/flash_build/build.log
+date > ./minerva/flash_build/build.log
+echo >> ./minerva/flash_build/build.log
+haxe -v -swf-version 9 -swf ./minerva/flash_build/app.swf -main cw.net.SocketBridge >> ./minerva/flash_build/build.log
 cp /med/builds/swfobject/swfobject/expressInstall.swf ./cwtools/exp/
