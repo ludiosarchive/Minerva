706e1bbb570b982677dcab2468f3daf63fd6f861
ivank <ivank@ludios.net>
2009-12-13 18:56:53 -0800

    comment auto-compile.sh

--- auto-compile.sh
+++ auto-compile.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# This script watches the (directory it is contained in) for modifications
+# and runs ./compile.sh when anything changes.
+
 cd "`dirname $0`"
 
 while inotifywait -e modify -e move -e create -r .; do
