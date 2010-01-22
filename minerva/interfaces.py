73386324fa8044b286445bd0f80999ba41d76536
ivank <ivank@ludios.net>
2010-01-22 07:03:54 -0800

    move another interface no one cares (ISimpleConsumer) about to interfaces.py

--- minerva/interfaces.py
+++ minerva/interfaces.py
@@ -1,6 +1,49 @@
 from zope.interface import Interface
 
 
+
+# Copy/paste of twisted.internet.interfaces.IConsumer, with 'write' method removed
+class ISimpleConsumer(Interface):
+	"""
+	A consumer consumes data from a producer.
+	"""
+
+	def registerProducer(producer, streaming):
+		"""
+		Register to receive data from a producer.
+
+		This sets self to be a consumer for a producer.  When this object runs
+		out of data (as when a send(2) call on a socket succeeds in moving the
+		last data from a userspace buffer into a kernelspace buffer), it will
+		ask the producer to resumeProducing().
+
+		For L{IPullProducer} providers, C{resumeProducing} will be called once
+		each time data is required.
+
+		For L{IPushProducer} providers, C{pauseProducing} will be called
+		whenever the write buffer fills up and C{resumeProducing} will only be
+		called when it empties.
+
+		@type producer: L{IProducer} provider
+
+		@type streaming: C{bool}
+		@param streaming: C{True} if C{producer} provides L{IPushProducer},
+		C{False} if C{producer} provides L{IPullProducer}.
+
+		@raise RuntimeError: If a producer is already registered.
+
+		@return: C{None}
+		"""
+
+
+	def unregisterProducer():
+		"""
+		Stop consuming data from a producer, without disconnecting.
+		"""
+
+
+
+
 class IStreamNotificationReceiver(Interface):
 	"""
 	Objects that implement this can get notified about new and dying Streams.
--- minerva/newlink.py
+++ minerva/newlink.py
@@ -6,6 +6,7 @@ See minerva/sample/demo.py for an idea of how to use the classes below.
 
 from minerva import abstract, decoders
 from minerva.website import RejectTransport
+from minerva.interfaces import ISimpleConsumer
 
 import simplejson
 import traceback
@@ -18,47 +19,6 @@ from twisted.python.failure import Failure
 from twisted.web import resource, http
 
 _postImportVars = vars().keys()
-
-
-# Copy/paste of twisted.internet.interfaces.IConsumer, with 'write' method removed
-class ISimpleConsumer(Interface):
-	"""
-	A consumer consumes data from a producer.
-	"""
-
-	def registerProducer(producer, streaming):
-		"""
-		Register to receive data from a producer.
-
-		This sets self to be a consumer for a producer.  When this object runs
-		out of data (as when a send(2) call on a socket succeeds in moving the
-		last data from a userspace buffer into a kernelspace buffer), it will
-		ask the producer to resumeProducing().
-
-		For L{IPullProducer} providers, C{resumeProducing} will be called once
-		each time data is required.
-
-		For L{IPushProducer} providers, C{pauseProducing} will be called
-		whenever the write buffer fills up and C{resumeProducing} will only be
-		called when it empties.
-
-		@type producer: L{IProducer} provider
-
-		@type streaming: C{bool}
-		@param streaming: C{True} if C{producer} provides L{IPushProducer},
-		C{False} if C{producer} provides L{IPullProducer}.
-
-		@raise RuntimeError: If a producer is already registered.
-
-		@return: C{None}
-		"""
-
-
-	def unregisterProducer():
-		"""
-		Stop consuming data from a producer, without disconnecting.
-		"""
-
 
 
 class BadFrame(Exception):
@@ -1561,6 +1521,7 @@ class HttpFace(resource.Resource):
 		d = request.notifyFinish()
 		d.addErrback(t.requestAborted)
 		t.requestStarted(request)
+
 
 
 from pypycpyo import optimizer
