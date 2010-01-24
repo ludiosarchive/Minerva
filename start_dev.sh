375525fac30c9d28c1eb07d46d3fed80f77c47ec
ivank <ivank@ludios.net>
2010-01-24 11:30:22 -0800

    start_dev.sh: make it slightly easier to change in the interface to listen on

--- start_dev.sh
+++ start_dev.sh
@@ -2,13 +2,14 @@
 export JSPATH="$HOME/JSPATH"
 export PYTHONPATH=$HOME/Projects/Coreweb:$HOME/Projects/Minerva:$HOME/Projects/Webmagic
 export PYRELOADING=1
+export INTERFACE=0
 
 echo "Using `which twistd`"
 
 looper python -OOOOO `which twistd` -r epoll -no minervarun \
 --secret "secret for testing only, do not use in production" \
--h tcp:8111:interface=0 \
--h ssl:444:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=0 \
--m tcp:8112:interface=0 \
--m tcp:843:interface=0 \
--m ssl:8113:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=0
+-h tcp:8111:interface=$INTERFACE \
+-h ssl:444:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=$INTERFACE \
+-m tcp:8112:interface=$INTERFACE \
+-m tcp:843:interface=$INTERFACE \
+-m ssl:8113:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=$INTERFACE
