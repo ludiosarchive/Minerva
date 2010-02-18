c70f4ba69173144cf6611c52cd775cc3acb8cca0
ivank <ivank@ludios.net>
2010-02-17 16:35:06 -0800

    compile.sh: copy expressInstall.swf from a slightly safer location

--- compile.sh
+++ compile.sh
@@ -8,4 +8,4 @@ PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
 date > ./minerva/flashtest/build.log
 echo >> ./minerva/flashtest/build.log
 haxe -v -swf-version 9 -swf ./minerva/flashtest/app.swf -main cw.net.FlashConnector >> ./minerva/flashtest/build.log
-cp /med/builds/swfobject/swfobject/expressInstall.swf ./minerva/flashtest/
+cp ~/source/swfobject/swfobject/expressInstall.swf ./minerva/flashtest/
