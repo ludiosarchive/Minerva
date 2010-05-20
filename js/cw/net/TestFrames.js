d3e6ddd112ca65a40ef0c1bae7f8bc301401d95f
ivank <ivank@ludios.net>
2010-05-20 23:43:42 +0000

    js/cw/net/TestFrames.js: cleanup: insert semicolons

--- js/cw/net/TestFrames.js
+++ js/cw/net/TestFrames.js
@@ -41,24 +41,24 @@ var repr = cw.repr.repr;
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'HelloFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new HelloFrame({"a": 1}), new HelloFrame({"a": 1}))
-		self.assertNotEqual(new HelloFrame({"a": 1}), new HelloFrame({"a": 2}))
-		self.assertNotEqual(new HelloFrame({"a": 1}), new HelloFrame({"a": 1, "b": 1}))
+		self.assertEqual(new HelloFrame({"a": 1}), new HelloFrame({"a": 1}));
+		self.assertNotEqual(new HelloFrame({"a": 1}), new HelloFrame({"a": 2}));
+		self.assertNotEqual(new HelloFrame({"a": 1}), new HelloFrame({"a": 1, "b": 1}));
 	},
 
 	function test_publicAttr(self) {
-		self.assertEqual({"anything": "goes"}, new HelloFrame({"anything": "goes"}).options)
+		self.assertEqual({"anything": "goes"}, new HelloFrame({"anything": "goes"}).options);
 	},
 
 	function test_repr(self) {
-		self.assertEqual('new HelloFrame({"a": 1})', repr(new HelloFrame({'a': 1})))
+		self.assertEqual('new HelloFrame({"a": 1})', repr(new HelloFrame({'a': 1})));
 	},
 
 	// TODO: decode tests
 
 	function test_encode(self) {
-		hello = new HelloFrame({transportNumber: 0})
-		self.assertEqual('{"tnum":0}' + 'H', hello.encode())
+		hello = new HelloFrame({transportNumber: 0});
+		self.assertEqual('{"tnum":0}' + 'H', hello.encode());
 
 	},
 
@@ -66,8 +66,8 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'HelloFrameTests').methods(
 	 * There's no real error checking if an invalid property is used.
 	 */
 	function test_encodeWithInvalidProperty(self) {
-		hello = new HelloFrame({aMadeUpKey: 0})
-		self.assertEqual('{"undefined":0}' + 'H', hello.encode())
+		hello = new HelloFrame({aMadeUpKey: 0});
+		self.assertEqual('{"undefined":0}' + 'H', hello.encode());
 	}
 );
 
@@ -76,28 +76,28 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'HelloFrameTests').methods(
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'StringFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new StringFrame("Hello"), new StringFrame("Hello"))
-		self.assertNotEqual(new StringFrame("Hello"), new StringFrame("Hello2"))
+		self.assertEqual(new StringFrame("Hello"), new StringFrame("Hello"));
+		self.assertNotEqual(new StringFrame("Hello"), new StringFrame("Hello2"));
 	},
 
 	function test_publicAttr(self) {
-		self.assertEqual("Hello", new StringFrame("Hello").string)
+		self.assertEqual("Hello", new StringFrame("Hello").string);
 	},
 
 	function test_repr(self) {
-		self.assertEqual('new StringFrame("Hello")', repr(new StringFrame("Hello")))
+		self.assertEqual('new StringFrame("Hello")', repr(new StringFrame("Hello")));
 	},
 
 	function test_decode(self) {
-		var s = '\x00unchecked\xfftext' + ' '
+		var s = '\x00unchecked\xfftext' + ' ';
 		self.assertEqual(
 			new StringFrame(s.substr(0, s.length - 1)),
-			StringFrame.decode(s))
+			StringFrame.decode(s));
 	},
 
 	function test_encode(self) {
-		var s = '\x00unchecked\xfftext'
-		self.assertEqual(s + ' ', new StringFrame(s).encode())
+		var s = '\x00unchecked\xfftext';
+		self.assertEqual(s + ' ', new StringFrame(s).encode());
 	}
 );
 
@@ -105,17 +105,17 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'StringFrameTests').methods(
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'SeqNumFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new SeqNumFrame(2), new SeqNumFrame(2))
-		self.assertNotEqual(new SeqNumFrame(2), new SeqNumFrame(3))
+		self.assertEqual(new SeqNumFrame(2), new SeqNumFrame(2));
+		self.assertNotEqual(new SeqNumFrame(2), new SeqNumFrame(3));
 	},
 
 	function test_publicAttr(self) {
-		self.assertEqual(2, new SeqNumFrame(2).seqNum)
+		self.assertEqual(2, new SeqNumFrame(2).seqNum);
 	},
 
 	function test_repr(self) {
-		self.assertEqual("new SeqNumFrame(2)", repr(new SeqNumFrame(2)))
-		self.assertEqual("new SeqNumFrame(" + Math.pow(2, 53) + ")", repr(new SeqNumFrame(Math.pow(2, 53))))
+		self.assertEqual("new SeqNumFrame(2)", repr(new SeqNumFrame(2)));
+		self.assertEqual("new SeqNumFrame(" + Math.pow(2, 53) + ")", repr(new SeqNumFrame(Math.pow(2, 53))));
 	},
 
 	function test_decode(self) {
@@ -124,13 +124,13 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'SeqNumFrameTests').methods(
 			String(1),
 			String(Math.pow(2, 32)),
 			String(Math.pow(2, 53))
-		]
+		];
 		goog.array.forEach(seqNumStrs, function(seqNumStr) {
 			var s = seqNumStr + 'N'
 			var seqNum = Number(seqNumStr);
 			self.assertEqual(
 				new SeqNumFrame(seqNum),
-				SeqNumFrame.decode(s))
+				SeqNumFrame.decode(s));
 		});
 	},
 
@@ -142,20 +142,20 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'SeqNumFrameTests').methods(
 			'00' + 'N',
 			goog.string.repeat('0', 1024),
 			' '
-		]
+		];
 
 		goog.array.forEach(strings, function(s) {
-			cw.UnitTest.logger.info('SeqNumFrameTests.test_decodeFailed: testing string ' + cw.repr.repr(s))
+			cw.UnitTest.logger.info('SeqNumFrameTests.test_decodeFailed: testing string ' + cw.repr.repr(s));
 			self.assertThrows(
 				InvalidFrame,
-				function() { SeqNumFrame.decode(s); })
+				function() { SeqNumFrame.decode(s); });
 		});
 	},
 
 	function test_encode(self) {
-		self.assertEqual('2N', new SeqNumFrame(2).encode())
-		self.assertEqual('0N', new SeqNumFrame(0).encode())
-		self.assertEqual(Math.pow(2, 53) + 'N', new SeqNumFrame(Math.pow(2, 53)).encode())
+		self.assertEqual('2N', new SeqNumFrame(2).encode());
+		self.assertEqual('0N', new SeqNumFrame(0).encode());
+		self.assertEqual(Math.pow(2, 53) + 'N', new SeqNumFrame(Math.pow(2, 53)).encode());
 	}
 );
 
@@ -163,63 +163,63 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'SeqNumFrameTests').methods(
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'SackFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new SackFrame(2, []), new SackFrame(2, []))
-		self.assertNotEqual(new SackFrame(2, [1]), new SackFrame(3, [2]))
-		self.assertNotEqual(new SackFrame(2, []), new SackFrame(3, []))
+		self.assertEqual(new SackFrame(2, []), new SackFrame(2, []));
+		self.assertNotEqual(new SackFrame(2, [1]), new SackFrame(3, [2]));
+		self.assertNotEqual(new SackFrame(2, []), new SackFrame(3, []));
 	},
 
 	function test_publicAttr(self) {
-		self.assertEqual(2, new SackFrame(2, [4, 5]).ackNumber)
-		self.assertEqual([4, 5], new SackFrame(2, [4, 5]).sackList)
+		self.assertEqual(2, new SackFrame(2, [4, 5]).ackNumber);
+		self.assertEqual([4, 5], new SackFrame(2, [4, 5]).sackList);
 	},
 
 	function test_repr(self) {
-		self.assertEqual("new SackFrame(2, [1, 4])", repr(new SackFrame(2, [1, 4])))
+		self.assertEqual("new SackFrame(2, [1, 4])", repr(new SackFrame(2, [1, 4])));
 	},
 
 	function test_decode(self) {
-		var s = '1,4|' + Math.pow(2, 53) + 'A'
+		var s = '1,4|' + Math.pow(2, 53) + 'A';
 		self.assertEqual(
 			new SackFrame(Math.pow(2, 53), [1, 4]),
-			SackFrame.decode(s))
+			SackFrame.decode(s));
 	},
 
 	function test_decodeNoSackNumbers(self) {
-		var s = '|' + Math.pow(2, 53) + 'A'
+		var s = '|' + Math.pow(2, 53) + 'A';
 		self.assertEqual(
 			new SackFrame(Math.pow(2, 53), []),
-			SackFrame.decode(s))
+			SackFrame.decode(s));
 	},
 
 	function test_decodeFailedAckNumberInvalid(self) {
 		goog.array.forEach([cw.net.LARGER_THAN_LARGEST_INTEGER_, -1, 0.5, 1.5], function(badNum) {
-			var s = '1,4|' + badNum + 'A'
+			var s = '1,4|' + badNum + 'A';
 			self.assertThrows(
 				InvalidFrame,
-				function() { SackFrame.decode(s); })
+				function() { SackFrame.decode(s); });
 		})
 	},
 
 	function test_decodeFailedOneSackNumberInvalid(self) {
 		goog.array.forEach([cw.net.LARGER_THAN_LARGEST_INTEGER_, -1, 0.5, 1.5], function(badNum) {
-			var s = '1,' + badNum + '|4A'
+			var s = '1,' + badNum + '|4A';
 			self.assertThrows(
 				InvalidFrame,
-				function() { SackFrame.decode(s); })
+				function() { SackFrame.decode(s); });
 		})
 	},
 
 	function test_decodeFailedTooManyPipes(self) {
-		var s = '||4A'
+		var s = '||4A';
 		self.assertThrows(
 			InvalidFrame,
-			function() { SackFrame.decode(s); })
+			function() { SackFrame.decode(s); });
 	},
 
 	function test_encode(self) {
-		self.assertEqual('1,4|2A', new SackFrame(2, [1, 4]).encode())
-		self.assertEqual('4|2A', new SackFrame(2, [4]).encode())
-		self.assertEqual('|2A', new SackFrame(2, []).encode())
+		self.assertEqual('1,4|2A', new SackFrame(2, [1, 4]).encode());
+		self.assertEqual('4|2A', new SackFrame(2, [4]).encode());
+		self.assertEqual('|2A', new SackFrame(2, []).encode());
 	}
 );
 
@@ -227,30 +227,30 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'SackFrameTests').methods(
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'YouCloseItFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new YouCloseItFrame(), new YouCloseItFrame())
-		self.assertNotEqual(new YouCloseItFrame(), [])
+		self.assertEqual(new YouCloseItFrame(), new YouCloseItFrame());
+		self.assertNotEqual(new YouCloseItFrame(), []);
 	},
 
 	function test_repr(self) {
-		self.assertEqual("new YouCloseItFrame()", repr(new YouCloseItFrame()))
+		self.assertEqual("new YouCloseItFrame()", repr(new YouCloseItFrame()));
 	},
 
 	function test_decode(self) {
-		var s = 'Y'
+		var s = 'Y';
 		self.assertEqual(
 			new YouCloseItFrame(),
-			YouCloseItFrame.decode(s))
+			YouCloseItFrame.decode(s));
 	},
 
 	function test_decodeFailed(self) {
-		var s = 'extra stuff' + 'Y'
+		var s = 'extra stuff' + 'Y';
 		self.assertThrows(
 			InvalidFrame,
-			function() { YouCloseItFrame.decode(s); })
+			function() { YouCloseItFrame.decode(s); });
 	},
 
 	function test_encode(self) {
-		self.assertEqual('Y', new YouCloseItFrame().encode())
+		self.assertEqual('Y', new YouCloseItFrame().encode());
 	}
 );
 
@@ -258,29 +258,29 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'YouCloseItFrameTests').methods
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'PaddingFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new PaddingFrame(4096), new PaddingFrame(4096))
-		self.assertNotEqual(new PaddingFrame(4096), new PaddingFrame(4097))
+		self.assertEqual(new PaddingFrame(4096), new PaddingFrame(4096));
+		self.assertNotEqual(new PaddingFrame(4096), new PaddingFrame(4097));
 	},
 
 	function test_publicAttr(self) {
-		self.assertEqual(4096, new PaddingFrame(4096).numBytes)
+		self.assertEqual(4096, new PaddingFrame(4096).numBytes);
 	},
 
 	function test_repr(self) {
-		self.assertEqual("new PaddingFrame(4096)", repr(new PaddingFrame(4096)))
+		self.assertEqual("new PaddingFrame(4096)", repr(new PaddingFrame(4096)));
 	},
 
 	function test_decode(self) {
-		var s = 'completely ignored stuff' + 'P'
-		var n = s.length - 1
+		var s = 'completely ignored stuff' + 'P';
+		var n = s.length - 1;
 		self.assertEqual(
 			new PaddingFrame(n),
-			PaddingFrame.decode(s))
+			PaddingFrame.decode(s));
 	},
 
 	function test_encode(self) {
-		self.assertEqual(goog.string.repeat(' ', 5) + 'P', new PaddingFrame(5).encode())
-		self.assertEqual('P', new PaddingFrame(0).encode())
+		self.assertEqual(goog.string.repeat(' ', 5) + 'P', new PaddingFrame(5).encode());
+		self.assertEqual('P', new PaddingFrame(0).encode());
 	}
 );
 
@@ -288,54 +288,54 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'PaddingFrameTests').methods(
 cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'ResetFrameTests').methods(
 
 	function test_eq(self) {
-		self.assertEqual(new ResetFrame("why", true), new ResetFrame("why", true))
-		self.assertNotEqual(new ResetFrame("why", true), new ResetFrame("why", false))
-		self.assertNotEqual(new ResetFrame("why2", true), new ResetFrame("why", true))
+		self.assertEqual(new ResetFrame("why", true), new ResetFrame("why", true));
+		self.assertNotEqual(new ResetFrame("why", true), new ResetFrame("why", false));
+		self.assertNotEqual(new ResetFrame("why2", true), new ResetFrame("why", true));
 	},
 
 	function test_publicAttr(self) {
-		self.assertEqual("why", new ResetFrame("why", true).reasonString)
-		self.assertEqual(true, new ResetFrame("why", true).applicationLevel)
+		self.assertEqual("why", new ResetFrame("why", true).reasonString);
+		self.assertEqual(true, new ResetFrame("why", true).applicationLevel);
 	},
 
 	function test_repr(self) {
-		self.assertEqual('new ResetFrame("why", true)', repr(new ResetFrame("why", true)))
+		self.assertEqual('new ResetFrame("why", true)', repr(new ResetFrame("why", true)));
 	},
 
 	function test_encode(self) {
-		self.assertEqual('the reason|0!', new ResetFrame("the reason", false).encode())
-		self.assertEqual('the reason|1!', new ResetFrame("the reason", true).encode())
+		self.assertEqual('the reason|0!', new ResetFrame("the reason", false).encode());
+		self.assertEqual('the reason|1!', new ResetFrame("the reason", true).encode());
 	},
 
 	function test_decode(self) {
 		goog.array.forEach([true, false], function(applicationLevel) {
 			goog.array.forEach(['the reason', 'the | | reason', '', '|', '||'], function(reasonString) {
-				var s = reasonString + '|' + String(Number(applicationLevel)) + '!'
+				var s = reasonString + '|' + String(Number(applicationLevel)) + '!';
 				cw.UnitTest.logger.info(
 					'ResetFrameTests.test_decode: testing '+
 					'applicationLevel=' + cw.repr.repr(applicationLevel) +
 					', reasonString=' + cw.repr.repr(reasonString) +
-					', s=' + cw.repr.repr(s))
+					', s=' + cw.repr.repr(s));
 				self.assertEqual(
 					new ResetFrame(reasonString, applicationLevel),
-					ResetFrame.decode(s))
+					ResetFrame.decode(s));
 			})
 		})
 	},
 
 	function test_decodeFailedBadReason(self) {
-		var s = '\x7freason|0!'
+		var s = '\x7freason|0!';
 		self.assertThrows(
 			InvalidFrame,
-			function() { ResetFrame.decode(s); })
+			function() { ResetFrame.decode(s); });
 
 	},
 
 	function test_decodeFailedBadBoolean(self) {
-		var s = 'reason|2!'
+		var s = 'reason|2!';
 		self.assertThrows(
 			InvalidFrame,
-			function() { ResetFrame.decode(s); })
+			function() { ResetFrame.decode(s); });
 	}
 );
 
@@ -345,42 +345,42 @@ cw.UnitTest.TestCase.subclass(cw.net.TestFrames, 'TransportKillFrameTests').meth
 	function test_eq(self) {
 		self.assertEqual(
 			new TransportKillFrame(tk.stream_attach_failure),
-			new TransportKillFrame(tk.stream_attach_failure))
+			new TransportKillFrame(tk.stream_attach_failure));
 		self.assertNotEqual(
 			new TransportKillFrame(tk.stream_attach_failure),
-			new TransportKillFrame(tk.acked_unsent_strings))
+			new TransportKillFrame(tk.acked_unsent_strings));
 	},
 
 	function test_publicAttr(self) {
 		self.assertEqual(
 			tk.acked_unsent_strings,
-			new TransportKillFrame(tk.acked_unsent_strings).reason)
+			new TransportKillFrame(tk.acked_unsent_strings).reason);
 	},
 
 	function test_repr(self) {
 		self.assertEqual(
 			'new TransportKillFrame("frame_corruption")',
-			repr(new TransportKillFrame(tk.frame_corruption)))
+			repr(new TransportKillFrame(tk.frame_corruption)));
 	},
 
 	function test_encode(self) {
-		self.assertEqual('frame_corruptionK', new TransportKillFrame(tk.frame_corruption).encode())
+		self.assertEqual('frame_corruptionK', new TransportKillFrame(tk.frame_corruption).encode());
 	},
 
 	function test_decode(self) {
 		goog.array.forEach(goog.object.getKeys(cw.net.AllTransportKillReasons_), function(reason) {
-			var s = reason + 'K'
+			var s = reason + 'K';
 			self.assertEqual(
 				new TransportKillFrame(reason),
-				TransportKillFrame.decode(s))
+				TransportKillFrame.decode(s));
 		})
 	},
 
 	function test_decodeFailed(self) {
-		var s = 'not_a_reasonK'
+		var s = 'not_a_reasonK';
 		self.assertThrows(
 			InvalidFrame,
-			function() { TransportKillFrame.decode(s); })
+			function() { TransportKillFrame.decode(s); });
 	}
 );
 
