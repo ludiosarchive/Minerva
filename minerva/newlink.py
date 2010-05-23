5e175f121011ac8a6cad7ba4a565c8cce4588292
ivank <ivank@ludios.net>
2010-05-23 22:38:16 +0000

    If Python Minerva server's receive window is overflowing, write out a SackFrame before writing out the TransportKillFrame

--- minerva/newlink.py
+++ minerva/newlink.py
@@ -937,11 +937,12 @@ class SocketTransport(object):
 		self._terminating = True
 
 
-	# No need for mailboxify because it just calls _closeWith
+	# No need for mailboxify because it calls _closeWith
 	def causedRwinOverflow(self):
 		"""
 		@see L{IMinervaTransport.causedRwinOverflow}
 		"""
+		self._toSend += self._encodeFrame(SackFrame(*self._stream.getSACK()))
 		self._closeWith(tk_rwin_overflow)
 
 
