d890fb184a1b9ed1dda619b15de61ba5ff9cec2b
ivank <ivank@ludios.net>
2010-06-06 06:57:34 +0000

    add stub js/cw/net/TestClient.js

---
+++ js/cw/net/TestClient.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview Tests for cw/net/client.js
+ */
+
+goog.provide('cw.net.TestClient');
+
+goog.require('cw.UnitTest');
+goog.require('cw.net.Stream');
+goog.require('cw.net.ClientTransport');
+
+
+// anti-clobbering for JScript
+(function(){
+
+cw.UnitTest.TestCase.subclass(cw.net.TestClient, 'ClientTransportTests').methods(
+	
+);
+
+})(); // end anti-clobbering for JScript
