ab87916faa96208e170c043265d71307a538cc2a
ivank <ivank@ludios.net>
2010-12-03 06:56:15 +0000

    runtests.sh: make MYPY_CONSTANT_BINDER_AUTOENABLE=0 explicit, in case it is already in the environment

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-                                  time python -N -W all `which trial` minerva
+MYPY_CONSTANT_BINDER_AUTOENABLE=0 time python -N -W all `which trial` minerva
 MYPY_CONSTANT_BINDER_AUTOENABLE=1 time python -N -W all `which trial` minerva
 
-                                  time pypy      -W all `which trial` minerva
+MYPY_CONSTANT_BINDER_AUTOENABLE=0 time pypy      -W all `which trial` minerva
 MYPY_CONSTANT_BINDER_AUTOENABLE=1 time pypy      -W all `which trial` minerva
