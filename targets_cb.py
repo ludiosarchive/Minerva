428e3831bdb38b96b8eb665c7c767eb2ba53f004
ivank <ivan@ludios.org>
2011-02-16 18:46:21 +0000

    Add targets_cb.py

---
+++ targets_cb.py
@@ -0,0 +1,8 @@
+# both outputs and breakers[n][1] are relative to this file's directory.
+
+targets = [
+	{"output": "js/cw/net/autocachebreakers.js",
+	"breakers": [
+		("cw.net.breaker_FlashConnector_swf", "minerva/compiled_client/FlashConnector.swf"),
+	]},
+]
