5fc7e31ab30f885724536b92768fd486c4ca2f8b
Ivan Kozik <ivan@ludios.org>
2011-05-09 08:49:30 +0000

    runtests.sh: Use correct REFBINDER env variable

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-MYPY_CONSTANT_BINDER_AUTOENABLE=0 time python -N -W all `which trial` minerva
-MYPY_CONSTANT_BINDER_AUTOENABLE=1 time python -N -W all `which trial` minerva
+REFBINDER_AUTOENABLE=0 time python -N -W all `which trial` minerva
+REFBINDER_AUTOENABLE=1 time python -N -W all `which trial` minerva
 
-MYPY_CONSTANT_BINDER_AUTOENABLE=0 time pypy      -W all `which trial` minerva
-MYPY_CONSTANT_BINDER_AUTOENABLE=1 time pypy      -W all `which trial` minerva
+REFBINDER_AUTOENABLE=0 time pypy      -W all `which trial` minerva
+REFBINDER_AUTOENABLE=1 time pypy      -W all `which trial` minerva
