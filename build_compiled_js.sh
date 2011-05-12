fd8a5a6580728a8bc9dc8e626e91f5bb5cb2ccdd
Ivan Kozik <ivan@ludios.org>
2011-05-12 09:33:01 +0000

    Fix log redirection in build_compiled_js.sh; rebuild

--- build_compiled_js.sh
+++ build_compiled_js.sh
@@ -25,11 +25,11 @@ COMMON="../closure-library/closure/bin/build/closurebuilder.py \
 
 $COMMON \
 --namespace="cw.net.bootstrap_XDRSetup" \
---output_file=minerva/compiled_client/bootstrap_XDRSetup.js
+--output_file=minerva/compiled_client/bootstrap_XDRSetup.js \
 2>&1 | tee minerva/compiled_client/bootstrap_XDRSetup.js.log
 
 $COMMON \
 --namespace="cw.net.setupXDRFrame" \
 --namespace="cw.net.XHRSlave" \
---output_file=minerva/compiled_client/xdrframe.js
+--output_file=minerva/compiled_client/xdrframe.js \
 2>&1 | tee minerva/compiled_client/xdrframe.js.log
