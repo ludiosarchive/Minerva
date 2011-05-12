3775b814088b774b8323aa991a549feadf21389a
Ivan Kozik <ivan@ludios.org>
2011-05-12 07:08:23 +0000

    build_swfs.sh: don't mess with HAXE_LIBRARY_PATH; don't put timestamps in log files; use a better filename for log file

--- build_swfs.sh
+++ build_swfs.sh
@@ -1,11 +1,6 @@
-#!/bin/zsh -e
+#!/bin/sh -e
 
-./regen-docs.sh
-
-export HAXE_LIBRARY_PATH=/usr/local/haxe/std:./hx
-export HAXE_HOME=/usr/local/haxe
-export BUILDLOG="./minerva/compiled_client/haxe_build.log"
-PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
-date > "$BUILDLOG"
-echo >> "$BUILDLOG"
-haxe -v -swf-version 9 -swf ./minerva/compiled_client/FlashConnector.swf -main cw.net.FlashConnector >> "$BUILDLOG"
+haxe -cp hx -v -swf-version 9 -swf \
+./minerva/compiled_client/FlashConnector.swf \
+-main cw.net.FlashConnector \
+> ./minerva/compiled_client/FlashConnector.swf.log
