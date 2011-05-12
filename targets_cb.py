2fa58fd6138c1c6c796db73b9018d781256fa4b3
Ivan Kozik <ivan@ludios.org>
2011-05-12 08:33:38 +0000

    Fix a path in targets_cb.py

--- targets_cb.py
+++ targets_cb.py
@@ -1,7 +1,7 @@
 # both outputs and breakers[n][1] are relative to this file's directory.
 
 targets = [
-	{"output": "js/cw/net/autocachebreakers.js",
+	{"output": "js_minerva/cw/net/autocachebreakers.js",
 	"breakers": [
 		("cw.net.breaker_FlashConnector_swf", "minerva/compiled_client/FlashConnector.swf"),
 	]},
