78a37cc84c864852e518278b8633b6fb78045d42
Ivan Kozik <ivan@ludios.org>
2011-06-07 21:14:45 +0000

    Improve runtests.sh; run CPython with flags that actually exist

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -e
 
-REFBINDER_AUTOENABLE=0 time python -N -W all `which trial` minerva
-REFBINDER_AUTOENABLE=1 time python -N -W all `which trial` minerva
+REFBINDER_AUTOENABLE=0 time python     -W all `which trial` minerva
+REFBINDER_AUTOENABLE=1 time python     -W all `which trial` minerva
+REFBINDER_AUTOENABLE=0 time python -O  -W all `which trial` minerva
+REFBINDER_AUTOENABLE=1 time python -O  -W all `which trial` minerva
+REFBINDER_AUTOENABLE=0 time python -OO -W all `which trial` minerva
+REFBINDER_AUTOENABLE=1 time python -OO -W all `which trial` minerva
 
-REFBINDER_AUTOENABLE=0 time pypy      -W all `which trial` minerva
-REFBINDER_AUTOENABLE=1 time pypy      -W all `which trial` minerva
+REFBINDER_AUTOENABLE=0 time pypy       -W all `which trial` minerva
+REFBINDER_AUTOENABLE=1 time pypy       -W all `which trial` minerva
+
+#REFBINDER_AUTOENABLE=0 time jython      `which trial` minerva
+#REFBINDER_AUTOENABLE=1 time jython      `which trial` minerva
