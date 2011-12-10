bc2e6fbfe51af6f677128597487991c55935f61c
Ivan Kozik <ivan@ludios.org>
2011-12-10 09:40:35 +0000

    docs/: Link stylesheet instead of embedding it

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 # This depends on docutils
-for i in docs/*.rst; do rst2html.py --tab-width=6 --stylesheet-path=docs/style.css --report=3 $i > docs/`basename $i .rst`.html; done
+for i in docs/*.rst; do rst2html.py --link-stylesheet --tab-width=6 --stylesheet-path=style.css --report=3 $i > docs/`basename $i .rst`.html; done
 
 ## rst2html.py --tab-width=6 --stylesheet-path=docs/style.css --warnings=_with_warnings_README.log README > _with_warnings_README.html 
