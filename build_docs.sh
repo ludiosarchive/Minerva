e08e5d1c9f4779d975c28acd6a15380df6a920e4
Ivan Kozik <ivan@ludios.org>
2011-12-10 09:43:07 +0000

    docs/: Fix newlines after generating .html files on Windows

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,15 @@
 #!/bin/sh -e
 
+# Windows users: this script can be run with MSYS bash.
+# Make sure you also have dos2unix.exe.
+
 # This depends on docutils
 for i in docs/*.rst; do rst2html.py --link-stylesheet --tab-width=6 --stylesheet-path=style.css --report=3 $i > docs/`basename $i .rst`.html; done
 
 ## rst2html.py --tab-width=6 --stylesheet-path=docs/style.css --warnings=_with_warnings_README.log README > _with_warnings_README.html 
+
+UNAME_O=`uname -o`
+if [[ $UNAME_O == 'Msys' || $UNAME_O == 'Cygwin' ]]; then
+	echo "Fixing newlines..."
+	dos2unix docs/*.html
+fi
