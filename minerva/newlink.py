6923211c7dc4401ddd8680dcdaabadaa29b9d897
Ivan Kozik <ivan@ludios.org>
2011-12-18 17:59:31 +0000

    Add a minerva.newlink for temporary backwards-compat

---
+++ minerva/newlink.py
@@ -0,0 +1,8 @@
+import warnings
+warnings.warn("minerva.newlink is deprecated and will be removed very soon.  "
+	"Import from minerva.mserver and minerva.interfaces instead.")
+
+from minerva.interfaces import *
+from minerva.mserver import *
+
+Stream = ServerStream
