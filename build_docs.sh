59b5944dad47cd6b385d42302bf63cbc88505cdb
Ivan Kozik <ivan@ludios.org>
2012-02-29 20:21:18 +0000

    build_docs.sh: Use tr instead of dos2unix

--- build_docs.sh
+++ build_docs.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Windows users: this script can be run with MSYS bash.
-# Make sure you also have dos2unix.exe.
+# Make sure you also have sed.exe and tr.exe.
 
 # This depends on docutils
 for i in docs/*.rst
@@ -12,13 +12,8 @@ for i in docs/*.rst
 		--stylesheet-path=style.css --report=3 $i \
 		| sed "s/<p><!-- expect dupe --><p /<p /g" \
 		| sed "s/<\/p><!-- expect dupe --><\/p>/<\/p>/g" \
+		| tr "\r\n" "\n" \
 		> docs/`basename $i .rst`.html
 done
 
 ## rst2html.py --tab-width=6 --stylesheet-path=docs/style.css --warnings=_with_warnings_README.log README > _with_warnings_README.html 
-
-UNAME_O=`uname -o`
-if [[ $UNAME_O == 'Msys' || $UNAME_O == 'Cygwin' ]]; then
-	echo "Fixing newlines..."
-	dos2unix docs/*.html
-fi
