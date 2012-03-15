d204a7c4df739ce4a6b243b1cf3cfa6ca69f6bf4
Ivan Kozik <ivan@ludios.org>
2012-03-15 03:18:41 +0000

    Add tutorial files standalone_2 and standalone_3

---
+++ minerva/tutorial/standalone_3.py
@@ -0,0 +1,119 @@
+# Since we're not using twistd, we need to set up logging.
+import sys
+from twisted.python import log
+log.startLogging(sys.stdout)
+
+from twisted.web import resource, server
+from twisted.internet import reactor
+
+from minerva.mserver import WebPort, StreamTracker
+
+try:
+	import simplejson as json
+except ImportError:
+	import json
+
+
+# This is the server-side protocol where you'll receive the client's strings.
+class DemoProtocol(object):
+	def streamStarted(self, stream):
+		self.stream = stream
+		self.id = self.factory.clientCounter
+		self.factory.clients.add(self)
+
+	def streamReset(self, reasonString, applicationLevel):
+		log.msg("Stream reset: %r" % (reasonString,))
+		self.factory.clients.remove(self)
+
+	def stringReceived(self, s):
+		text = json.loads(s)
+		print "Received from client #%d: %r" % (self.id, text)
+		for protocol in self.factory.clients:
+			if protocol == self:
+				protocol.stream.sendString(
+					json.dumps(u"You said: %s" % (text,)))
+			else:
+				protocol.stream.sendString(
+					json.dumps(u"Client #%d said: %s" % (self.id, text)))
+
+
+# The factory can be used to store data and instantiate objects
+# shared by the connected protocols.
+class DemoFactory(object):
+	def __init__(self):
+		self.clientCounter = 0
+		self.clients = set()
+
+	def buildProtocol(self):
+		self.clientCounter += 1
+		stream = DemoProtocol()
+		stream.factory = self
+		return stream
+
+
+class MyDemo(resource.Resource):
+	def render_GET(self, request):
+		return r"""<!doctype html>
+<div id="minerva-elements"></div> <!-- required container -->
+
+Send Unicode string to everyone:
+<input type="text" id="textinput">
+<input type="button" value="Send" onclick="sendString(byId('textinput').value)">
+
+<p>If it's not working, look at your browser's Console
+(Chrome: <kbd>F12</kbd> -> Console tab; Firefox: <kbd>ctrl-shift-k</kbd>)</p>
+
+<pre id="output"></pre>
+
+<script src="/_minerva/minerva-client.js"></script>
+<script>
+var byId = function(id) { return document.getElementById(id); };
+var logMessage = function(msg) {
+	byId('output').appendChild(document.createTextNode(msg + "\n"));
+	if(window.console && console.log) { console.log(msg); }
+};
+window.onerror = logMessage;
+
+var DemoProtocol = function() {};
+DemoProtocol.prototype.streamStarted = function(stream) {
+	this.stream = stream;
+};
+DemoProtocol.prototype.streamReset = function(reasonString, applicationLevel) {
+	logMessage("Stream reset: " + reasonString);
+};
+DemoProtocol.prototype.stringReceived = function(s) {
+	logMessage("Received from server: " + Minerva.JSON.parse(s));
+};
+
+var sendString = function(s) {
+	stream.sendString(Minerva.JSON.serialize(s));
+};
+
+var protocol = new DemoProtocol();
+var stream = new Minerva.ClientStream(new Minerva.HttpEndpoint("/_minerva/"));
+stream.bindToProtocol(protocol);
+stream.start();
+</script>
+"""
+
+
+class Root(resource.Resource):
+	def __init__(self, webPort):
+		resource.Resource.__init__(self)
+		self.putChild('', MyDemo())
+		self.putChild('_minerva', webPort)
+
+
+def makeSite():
+	clock = reactor
+	factory = DemoFactory()
+	tracker = StreamTracker(clock, factory)
+	webPort = WebPort(clock, tracker)
+	root = Root(webPort)
+	# timeout= is twisted.web's keep-alive timeout that determines how long
+	# before the server disconnects HTTP connections with no active requests.
+	return server.Site(root, timeout=75)
+
+
+reactor.listenTCP(10001, makeSite())
+reactor.run()
