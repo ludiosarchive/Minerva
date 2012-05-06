048d2fd2cbceb2c2159c7fc16dfbea4248f49e45
Ivan Kozik <ivan@ludios.org>
2012-05-06 17:02:58 +0000

    Write out the Coreweb version used during the JS build, just like the old bash script did

--- build_compiled_js.py
+++ build_compiled_js.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from coreweb._closurebuild.compiler import compile
+from coreweb._closurebuild.compiler import compile, get_git_rev
 
 roots = ["../Coreweb/js_coreweb", "js_minerva"]
+extra_lines = ["Used Coreweb " + get_git_rev("../Coreweb")]
 
 compile(
 	 roots=roots
@@ -10,6 +11,7 @@ compile(
 	,output="minerva/compiled_client/xdrframe.js"
 	,output_log="minerva/compiled_client/xdrframe.js.log"
 	,externs=['js_minerva/externs/standalone-client.js']
+	,log_lines=extra_lines
 )
 
 compile(
@@ -19,4 +21,5 @@ compile(
 	,output_log="minerva/compiled_client/minerva-client.js.log"
 	,externs=['js_minerva/externs/standalone-client.js']
 	,defines={'cw.net.STANDALONE_CLIENT_BUILD_': 'true'}
+	,log_lines=extra_lines
 )
