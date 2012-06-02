761f06e0106902e14631f3e7a3a92b0b93b1838f
Ivan Kozik <ivan@ludios.org>
2012-06-02 10:58:47 +0000

    Remove deprecated modules {newlink,website}.  Use {mserver,mutils} instead.

--- minerva/__init__.py
+++ minerva/__init__.py
@@ -1,2 +1,2 @@
 # Remember to update js_minerva/__init__.py, too
-__version__ = '12.4.24'
+__version__ = '12.6.2'
--- minerva/website.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn("minerva.website is deprecated and will be removed very soon.  "
-	"Import from minerva.mutils instead.")
-
-from minerva.mutils import *
